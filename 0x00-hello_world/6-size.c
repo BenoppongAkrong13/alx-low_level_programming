3ba1fd166fbe1df46ff35d90fb19725457eb97f1
BenoppongAkrong13 <benjaminoppongakrong@gmail.com>
2023-06-15 20:39:36 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: always 0
+*/ 
+
+int main(void)
+{
+      char c;
+      int i;
+      long l;
+      long long ll;
+      float f;    
+ 
+      printf("Size 0f a char: %lu byte(s)\n", sizeof(acharacter));
+      printf("Size 0f an int: %lu byte(s)\n", sizeof(ainteger));
+      printf("Size 0f a long int: %lu byte(s)\n", sizeof(along));
+      printf("Size 0f a long long int: %lu byte(s)\n", sizeof(alonglong));
+      printf("Size 0f a float: %lu byte(s)\n", sizeof(afloat));
+
+       return (0);
+}
+
